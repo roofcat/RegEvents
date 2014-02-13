222ad41057baa624299d0fd17e4283ccdef26679
Christian Rojas <christiandiegor@gmail.com>
2014-02-13 12:42:58 -0300

    segunda subida

--- centralmonitoreo/settings.py
+++ centralmonitoreo/settings.py
@@ -88,4 +88,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR.child('static')

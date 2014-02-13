1498a0ca6f1b933116a61b9c2fa7bd2fcf9eee06
Christian Rojas <christiandiegor@gmail.com>
2014-02-13 16:03:55 -0300

    agregado archivos estaticos

--- centralmonitoreo/settings.py
+++ centralmonitoreo/settings.py
@@ -87,5 +87,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_URL = '/static/'   
 
-STATIC_URL = BASE_DIR.child('static')
+STATICFILES_DIRS = (  
+	BASE_DIR.child("static"),
+)

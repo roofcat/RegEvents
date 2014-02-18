a1ef9ed9411bf03a0b5011e42a8953a5ff053a5d
Christian Rojas <christiandiegor@gmail.com>
2014-02-18 19:54:06 -0300

    se agrego formulario de agregar registro

--- central/forms.py
+++ central/forms.py
@@ -1,13 +1,14 @@
+from django import forms
 from django.forms import ModelForm
 
 
-from central.models import Evento, Sucursal, Clave
+from central.models import Evento
                  
 
 class EventoForm(ModelForm):
 	
 	class Meta:
 		model = Evento
-		exclude = ['usuario']
+		exclude = ['usuario',]
 
 
--- central/views.py
+++ central/views.py
@@ -4,20 +4,25 @@ from django.template import RequestContext
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import FormView, TemplateView
              
 from django.utils import timezone
 from central.models import Evento
 from central.forms import EventoForm
+
 
 @login_required
 def nuevoevento(request):
 	if request == 'POST':
 		formulario = EventoForm(request.POST)
 		if formulario.is_valid():
-			evento = formulario.save(commit = False)
-			evento.usuario = request.user
+			evento = formulario.save(commit=False)
+			evento.usuario = request.usuario
 			evento.save()
 			return HttpResponseRedirect('/nuevoevento')
 	else:
 		formulario = EventoForm()
-	return render_to_response('nuevoevento.html',{'formulario':formulario},context_instance=RequestContext(request))
+	return render_to_response('nuevoevento.html',{'formulario':formulario},context_instance=RequestContext(request, locals()))
+
+

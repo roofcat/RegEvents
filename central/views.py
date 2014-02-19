541e75958a240ba84a31d4f08b94a6743a5a5652
Christian Rojas <christiandiegor@gmail.com>
2014-02-19 17:03:10 -0300

    modificaciones varias

--- central/views.py
+++ central/views.py
@@ -1,28 +1,22 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse 
 from django.template import RequestContext
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse_lazy
-from django.views.generic import FormView, TemplateView
              
-from django.utils import timezone
-from central.models import Evento
-from central.forms import EventoForm
+
+from central.forms import EventoForms
 
 
-@login_required
 def nuevoevento(request):
 	if request == 'POST':
-		formulario = EventoForm(request.POST)
-		if formulario.is_valid():
-			evento = formulario.save(commit=False)
-			evento.usuario = request.usuario
+		evento_form = EventoForm(request.POST)
+		if evento_form.is_valid():
+			evento = evento_form.save(commit=False)
+			evento.usuario = request.user
 			evento.save()            
 			return HttpResponseRedirect('/nuevoevento')
 	else:
-		formulario = EventoForm()
-	return render_to_response('nuevoevento.html',{'formulario':formulario},context_instance=RequestContext(request))
+		evento_form = EventoForm()
+	return render_to_response('nuevoevento.html',{'evento_form':evento_form},context_instance=RequestContext(request,locals()))
 
 
